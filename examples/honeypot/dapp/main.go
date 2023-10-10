9f5cee77213b3e7b83fe4dab19a1f0d113cf8390
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-10-10 10:05:36 -0300

    Improve honeypot error for unsupported deposits

--- examples/honeypot/dapp/main.go
+++ examples/honeypot/dapp/main.go
@@ -23,23 +23,34 @@ func (c HoneypotContract) Decoders() []eggroll.Decoder {
 }
 
 func (c *HoneypotContract) Advance(env *eggroll.Env, input any) ([]byte, error) {
-	// Handle Ether deposits
-	if deposit, ok := env.Deposit().(*wallets.EtherDeposit); ok {
+	if deposit := env.Deposit(); deposit != nil {
+		return c.handleDeposit(env, deposit)
+	}
+
+	return c.handleInput(env, input)
+}
+
+func (c *HoneypotContract) handleDeposit(env *eggroll.Env, deposit wallets.Deposit) ([]byte, error) {
+	switch deposit := env.Deposit().(type) {
+	case *wallets.EtherDeposit:
 		env.Logf("received deposit: %v\n", deposit)
 		if env.Sender() != honeypot.Owner {
 			// Transfer Ether deposits to honeypot.Owner
 			env.EtherTransfer(env.Sender(), honeypot.Owner, &deposit.Value)
 		}
-		ownerBalance := env.EtherBalanceOf(honeypot.Owner)
-		return ownerBalance.Bytes(), nil
-	}
+		return c.getBalance(env), nil
 
-	// Ignore inputs that are not from honeypot.Owner
+	default:
+		return nil, fmt.Errorf("unsupported deposit: %v", deposit)
+	}
+}
+
+func (c *HoneypotContract) handleInput(env *eggroll.Env, input any) ([]byte, error) {
 	if env.Sender() != honeypot.Owner {
+		// Ignore inputs that are not from honeypot.Owner
 		return nil, fmt.Errorf("ignoring input from %v", env.Sender())
 	}
 
-	// Handle honeypot.Owner withdraw request
 	switch input := input.(type) {
 	case *honeypot.Withdraw:
 		_, err := env.EtherWithdraw(honeypot.Owner, input.Value)
@@ -47,12 +58,16 @@ func (c *HoneypotContract) Advance(env *eggroll.Env, input any) ([]byte, error) 
 			return nil, err
 		}
 		env.Logf("withdraw %v\n", input.Value.ToBig().String())
-		ownerBalance := env.EtherBalanceOf(honeypot.Owner)
-		return ownerBalance.Bytes(), nil
+		return c.getBalance(env), nil
 
 	default:
 		return nil, fmt.Errorf("invalid input")
 	}
+}
+
+func (c *HoneypotContract) getBalance(env *eggroll.Env) []byte {
+	ownerBalance := env.EtherBalanceOf(honeypot.Owner)
+	return ownerBalance.Bytes()
 }
 
 func main() {
