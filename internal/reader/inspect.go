e1e8425a2d003c8480e33b8ffde7c4079adcb35e
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-10-13 15:46:25 -0300

    Add inspect to client

---
+++ internal/reader/inspect.go
@@ -0,0 +1,108 @@
+// Copyright (c) Gabriel de Quadros Ligneul
+// SPDX-License-Identifier: MIT (see LICENSE)
+
+package reader
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+// Client for the reader node inspect API.
+type InspectClient struct {
+	endpoint string
+}
+
+// Create a new inspect client.
+// The endpoint is for the reader node inspect HTTP API.
+func NewInspectClient(endpoint string) *InspectClient {
+	return &InspectClient{
+		endpoint: endpoint,
+	}
+}
+
+// Send a inspect request with the given payload.
+func (c *InspectClient) Inspect(ctx context.Context, payload []byte) (*InspectResult, error) {
+
+	// Prepare the request
+	req, err := http.NewRequest(http.MethodPost, c.endpoint, bytes.NewBuffer(payload))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req = req.WithContext(ctx)
+
+	// Make the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to perform request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read body: %v", err)
+		}
+		msg := "inspect error (status %v): %v"
+		return nil, fmt.Errorf(msg, resp.StatusCode, string(body))
+	}
+
+	// Decode the json response
+	var jsonResponse struct {
+		Status  string `json:"status"`
+		Reports []struct {
+			Payload string `json:"payload"`
+		} `json:"reports"`
+		ProcessedInputCount int
+	}
+	decoder := json.NewDecoder(resp.Body)
+	decoder.Decode(&jsonResponse)
+
+	// Build the result
+	status, err := convertInspectStatus(jsonResponse.Status)
+	if err != nil {
+		return nil, err
+	}
+	var reports []Report
+	for _, jsonReport := range jsonResponse.Reports {
+		payload, err := hexutil.Decode(jsonReport.Payload)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode report payload: %v", err)
+		}
+		report := Report{
+			Payload: payload,
+		}
+		reports = append(reports, report)
+	}
+	result := &InspectResult{
+		Status:              status,
+		Reports:             reports,
+		ProcessedInputCount: jsonResponse.ProcessedInputCount,
+	}
+	return result, nil
+}
+
+func convertInspectStatus(str string) (CompletionStatus, error) {
+	statusMap := map[string]CompletionStatus{
+		"Unprocessed":                CompletionStatusUnprocessed,
+		"Accepted":                   CompletionStatusAccepted,
+		"Rejected":                   CompletionStatusRejected,
+		"Exception":                  CompletionStatusException,
+		"Machine_halted":             CompletionStatusMachineHalted,
+		"CycleLimitExceeded":         CompletionStatusCycleLimitExceeded,
+		"TimeLimitExceeded":          CompletionStatusTimeLimitExceeded,
+		"PayloadLengthLimitExceeded": CompletionStatusPayloadLengthLimitExceeded,
+	}
+	status, ok := statusMap[str]
+	if !ok {
+		return status, fmt.Errorf("invalid completion status: %v", str)
+	}
+	return status, nil
+}
--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -4,16 +4,10 @@
 package reader
 
 import (
-	"context"
 	"fmt"
-	"net/http"
-	"strconv"
-	"strings"
 	"time"
 
-	"github.com/Khan/genqlient/graphql"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 type Input struct {
@@ -64,6 +58,13 @@ type Report struct {
 	Payload     []byte
 }
 
+type InspectResult struct {
+	Status              CompletionStatus
+	Reports             []Report
+	Exception           []byte
+	ProcessedInputCount int
+}
+
 // Error when an object is not found.
 type NotFound struct {
 	typeName string
@@ -72,136 +73,3 @@ type NotFound struct {
 func (e NotFound) Error() string {
 	return fmt.Sprintf("%v not found", e.typeName)
 }
-
-// Given the GraphQL error message, check whether the error should be NotFound.
-func checkNotFound(typeName string, err error) error {
-	if strings.HasSuffix(err.Error(), "not found\n") {
-		return NotFound{typeName}
-	}
-	return err
-}
-
-// Read the rollups state by connecting to the rollups node GraphQL API.
-type GraphQLReader struct {
-	client graphql.Client
-}
-
-// Create a new GraphQL reader given the endpoint.
-func NewGraphQLReader(endpoint string) *GraphQLReader {
-	client := graphql.NewClient(endpoint, http.DefaultClient)
-	return &GraphQLReader{
-		client: client,
-	}
-}
-
-// Get an input from the rollups node.
-func (r *GraphQLReader) Input(ctx context.Context, index int) (*Input, error) {
-	_ = `# @genqlient
-	query getInput($inputIndex: Int!) {
-	  input(index: $inputIndex) {
-	    status
-	    payload
-	    msgSender
-	    timestamp
-	    blockNumber
-	    vouchers {
-	      edges {
-		node {
-		  index
-		  destination
-		  payload
-		}
-	      }
-	    }
-	    notices {
-	      edges {
-		node {
-		  index
-		  payload
-		}
-	      }
-	    }
-	    reports {
-	      edges {
-		node {
-		  index
-		  payload
-		}
-	      }
-	    }
-	  }
-	}`
-
-	resp, err := getInput(ctx, r.client, index)
-	if err != nil {
-		return nil, checkNotFound("input", err)
-	}
-
-	var input Input
-	input.Index = index
-	input.Status = resp.Input.Status
-
-	input.Payload, err = hexutil.Decode(resp.Input.Payload)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode payload: %v", err)
-	}
-
-	sender, err := hexutil.Decode(resp.Input.MsgSender)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode msgSender: %v", err)
-	}
-	input.Sender = common.Address(sender)
-
-	input.BlockNumber, err = strconv.ParseInt(resp.Input.BlockNumber, 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode block number: %v", err)
-	}
-
-	timestamp, err := strconv.ParseInt(resp.Input.BlockNumber, 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode timestmap: %v", err)
-	}
-	input.BlockTimestamp = time.Unix(timestamp, 0)
-
-	for _, edge := range resp.Input.Vouchers.Edges {
-		var voucher Voucher
-		voucher.InputIndex = index
-		voucher.OutputIndex = edge.Node.Index
-		destination, err := hexutil.Decode(edge.Node.Destination)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode voucher destination: %v", err)
-		}
-		voucher.Destination = common.Address(destination)
-		voucher.Payload, err = hexutil.Decode(edge.Node.Payload)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode voucher payload: %v", err)
-		}
-		input.Vouchers = append(input.Vouchers, voucher)
-	}
-
-	for _, edge := range resp.Input.Notices.Edges {
-		var notice Notice
-		notice.InputIndex = index
-		notice.OutputIndex = edge.Node.Index
-		notice.Payload, err = hexutil.Decode(edge.Node.Payload)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode notice payload: %v", err)
-		}
-		input.Notices = append(input.Notices, notice)
-	}
-
-	for _, edge := range resp.Input.Reports.Edges {
-		var report Report
-		report.InputIndex = index
-		report.OutputIndex = edge.Node.Index
-		report.Payload, err = hexutil.Decode(edge.Node.Payload)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode report payload: %v", err)
-		}
-		input.Reports = append(input.Reports, report)
-	}
-
-	return &input, nil
-}
-
-//go:generate go run github.com/Khan/genqlient
