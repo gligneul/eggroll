f5005d896cd727b594aa58f18b5944cb7d8cd93d
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-10-27 11:36:32 -0300

    Improve eth client test

--- pkg/eggeth/client_test.go
+++ pkg/eggeth/client_test.go
@@ -22,30 +22,7 @@ func TestClient(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
 	defer cancel()
 
-	// We use the sunodo devnet docker image to test the client.
-	// This image starts an anvil node with the Rollups contracts already deployed.
-	req := testcontainers.ContainerRequest{
-		Image: "sunodo/devnet:1.1.1",
-		Cmd: []string{
-			"anvil",
-			"--block-time",
-			"1",
-			"--load-state",
-			"/usr/share/sunodo/anvil_state.json",
-		},
-		Env: map[string]string{
-			"ANVIL_IP_ADDR": "0.0.0.0",
-		},
-		ExposedPorts: []string{"8545/tcp"},
-		WaitingFor:   wait.ForLog("Listening on 0.0.0.0:8545"),
-	}
-	anvilContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	if err != nil {
-		t.Fatalf("failed to start anvil container: %v", err)
-	}
+	anvilContainer := setupEthContainer(t, ctx)
 	defer func() {
 		if err := anvilContainer.Terminate(ctx); err != nil {
 			t.Fatalf("failed to terminate anvil container: %v", err)
@@ -76,25 +53,115 @@ func TestClient(t *testing.T) {
 		t.Fatalf("failed to create signer: %v", err)
 	}
 
-	// Test SendInput
-	payload := common.Hex2Bytes("deadbeef")
-	inputIndex, err := client.SendInput(ctx, signer, payload)
+	// Test client
+	testCases := []struct {
+		name   string
+		do     func(context.Context, *ETHClient, Signer) (int, error)
+		sender common.Address
+		input  []byte
+	}{
+		{
+			name: "SendInput",
+			do: func(ctx context.Context, c *ETHClient, s Signer) (int, error) {
+				return c.SendInput(ctx, s, common.Hex2Bytes("deadbeef"))
+			},
+			sender: common.HexToAddress("f39fd6e51aad88f6f4ce6ab8827279cfffb92266"),
+			input:  common.Hex2Bytes("deadbeef"),
+		},
+		{
+			name: "SendDAppAddress",
+			do: func(ctx context.Context, c *ETHClient, s Signer) (int, error) {
+				return c.SendDAppAddress(ctx, s)
+			},
+			sender: AddressDAppAddressRelay,
+			input:  client.dappAddress[:],
+		},
+		{
+			name: "SendEther",
+			do: func(ctx context.Context, c *ETHClient, s Signer) (int, error) {
+				return c.SendEther(ctx, s, big.NewInt(0xfafa), common.Hex2Bytes("deadbeef"))
+			},
+			sender: AddressEtherPortal,
+			input: common.Hex2Bytes("" +
+				// sender address
+				"f39fd6e51aad88f6f4ce6ab8827279cfffb92266" +
+				// value
+				"000000000000000000000000000000000000000000000000000000000000fafa" +
+				// payload
+				"deadbeef",
+			),
+		},
+		// TODO the erc20 test doesn't work because we need to deploy a
+		// erc20 contract first
+		//{
+		//	name: "SendERC20Tokens",
+		//	do: func(ctx context.Context, c *ETHClient, s Signer) (int, error) {
+		//		token := common.HexToAddress("beefbeefbeefbeefbeefbeefbeefbeefbeefbeef")
+		//		amount := big.NewInt(0xfafa)
+		//		input := common.Hex2Bytes("deadbeef")
+		//		return c.SendERC20Tokens(ctx, s, token, amount, input)
+		//	},
+		//	sender: AddressEtherPortal,
+		//	input: common.Hex2Bytes("" +
+		//		// token
+		//		"beefbeefbeefbeefbeefbeefbeefbeefbeefbeef" +
+		//		// sender address
+		//		"f39fd6e51aad88f6f4ce6ab8827279cfffb92266" +
+		//		// amount
+		//		"000000000000000000000000000000000000000000000000000000000000fafa" +
+		//		// payload
+		//		"deadbeef",
+		//	),
+		//},
+	}
+	for i, testCase := range testCases {
+		t.Logf("testing client.%v", testCase.name)
+		inputIndex, err := testCase.do(ctx, client, signer)
+		if err != nil {
+			t.Fatalf("failed to send: %v", err)
+		}
+		if inputIndex != i {
+			t.Fatalf("wrong input index: %v; expected: %v", inputIndex, i)
+		}
+		readSender, readInput, err := getInput(client.inputBox, client.dappAddress, inputIndex)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if readSender != testCase.sender {
+			t.Fatalf("wrong sender: %x", readSender)
+		}
+		if !bytes.Equal(readInput, testCase.input) {
+			t.Fatalf("wrong input: %x", readInput)
+		}
+	}
+}
+
+// We use the sunodo devnet docker image to test the client.
+// This image starts an anvil node with the Rollups contracts already deployed.
+func setupEthContainer(t *testing.T, ctx context.Context) testcontainers.Container {
+	req := testcontainers.ContainerRequest{
+		Image: "sunodo/devnet:1.1.1",
+		Cmd: []string{
+			"anvil",
+			"--block-time",
+			"1",
+			"--load-state",
+			"/usr/share/sunodo/anvil_state.json",
+		},
+		Env: map[string]string{
+			"ANVIL_IP_ADDR": "0.0.0.0",
+		},
+		ExposedPorts: []string{"8545/tcp"},
+		WaitingFor:   wait.ForLog("Listening on 0.0.0.0:8545"),
+	}
+	anvilContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
 	if err != nil {
-		t.Fatalf("failed to send input: %v", err)
+		t.Fatalf("failed to start anvil container: %v", err)
 	}
-	if inputIndex != 0 {
-		t.Fatalf("wrong input index: %v", inputIndex)
-	}
-	readSender, readPayload, err := getInput(client.inputBox, dappAddress, inputIndex)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if readSender != common.HexToAddress("f39fd6e51aad88f6f4ce6ab8827279cfffb92266") {
-		t.Fatalf("wrong sender: %x", readSender)
-	}
-	if !bytes.Equal(readPayload, payload) {
-		t.Fatalf("wrong payload: %x", readPayload)
-	}
+	return anvilContainer
 }
 
 func getInput(
